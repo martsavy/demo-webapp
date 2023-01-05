df185fe3f5a5fb1a67b1d412639533d2e26d22f3
Aliaksandr Martsavy <aliaksandr.martsavy@cfainstitute.org>
2023-01-05 15:21:30 +0100

    For check deployments from TeamCity added display DeployVersion.

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 def main():
     if environ.get('Foo') is not None:
         str = "Exist var 'Foo' with value:" + environ.get('Foo')
+        if environ.get('DeployVersion') is not None:
+            str = str + "<br>DeployedVersion is:" + environ.get('DeployVersion')
         return str
     else: 
         return "Var 'Foo' not exist"
